7d4b4530274a3196469669bfb370d028e2e36a86
Richard Hallgren <richard.hallgren@gmail.com>
2016-07-30 14:47:58 +0200

    Some cleaning in the food class and switching to properties instead of fields

--- ConsoleSnake/Food.cs
+++ ConsoleSnake/Food.cs
@@ -1,26 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ConsoleSnake
 {
     internal class Food
     {
-        public int XPosition;
-        public int YPostion;
-        Snake _snake;
+        public int XPosition { get; private set; }
+        public int YPostion { get; private set; }
 
         public void MakeNewFood()
         {
-            Random y = new Random();
-            YPostion = y.Next(Grid.StartY + 1, Grid.EndY - 1);
-            Random x = new Random();
-            XPosition = x.Next(Grid.StartX + 1, Grid.EndX - 1);
-            
+            Random yRandom = new Random();
+            YPostion = yRandom.Next(Grid.StartY + 1, Grid.EndY - 1);
+            Random xRandom = new Random();
+            XPosition = xRandom.Next(Grid.StartX + 1, Grid.EndX - 1);
         }
 
         public Food()
         {
-           MakeNewFood();
+            MakeNewFood();
         }
     }
 }
